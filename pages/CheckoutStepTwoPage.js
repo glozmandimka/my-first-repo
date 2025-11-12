615de69aa64048ade6093fb1160c95a89af9ae1b
Dmitry Glozman <glozmandimka@gmail.com>
2025-11-13 01:18:03 +0300

    feat: Add CheckoutStepTwoPage with finishCheckout method

---
+++ pages/CheckoutStepTwoPage.js
@@ -0,0 +1,11 @@
+class CheckoutStepTwoPage {
+  constructor(page) {
+    this.page = page;
+    this.itemNames = page.locator('[data-test="inventory-item-name"]');
+    this.totalPrice = page.locator('[data-test="total-label"]');
+    this.finishButton = page.locator('[data-test="finish"]');
+  }
+  async finishCheckout() {
+    await this.finishButton.click();
+  }
+}

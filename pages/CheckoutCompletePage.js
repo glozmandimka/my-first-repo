0c71f2006cf8a731f8fcbe1bd1a0dab7d26217c3
Dmitry Glozman <glozmandimka@gmail.com>
2025-11-13 01:28:08 +0300

    feat: Add CheckoutCompletePage with getCompletionMessage method

---
+++ pages/CheckoutCompletePage.js
@@ -0,0 +1,10 @@
+class CheckoutCompletePage {
+  constructor(page) {
+    this.page = page;
+    this.completionMessage = page.locator('[data-test="complete-header"]');
+    this.backHomeButton = page.locator('[data-test="back-to-products"]');
+  }
+  async getCompletionMessage() {
+    return await this.completionMessage.textContent();
+  }
+}

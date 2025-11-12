930b5ec4f6c6feb6741194219382418fb54f9cbe
Dmitry Glozman <glozmandimka@gmail.com>
2025-11-13 00:47:09 +0300

    fear: Add InventoryPage with addItemToCart, openCart and getPageTitle

---
+++ pages/InventoryPage.js
@@ -0,0 +1,24 @@
+class InventoryPage {
+  constructor(page) {
+    this.page = page;
+    this.pageTitle = page.locator("[data-test='title']");
+    this.cartIcon = page.locator("[data-test='shopping-cart-link']");
+    this.productItems = page.locator("[data-test='inventory-item']");
+    this.addToCartButtons = page.locator(".btn_primary");
+  }
+
+  async addItemToCart(itemName) {
+    const itemContainer = this.page.locator(".inventory_item").filter({
+      has: this.page.locator(".inventory_item_name", { hasText: itemName }),
+    });
+    await itemContainer.locator(".btn_primary").click();
+  }
+
+  async openCart() {
+    await this.cartIcon.click();
+  }
+
+  async getPageTitle() {
+    return await this.pageTitle.textContent();
+  }
+}
